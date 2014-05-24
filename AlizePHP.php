89e861cefac96e5402bdc1e84e3e0a997888200e
Héctor <hctpbl@gmail.com>
2014-05-24 17:23:19 +0200

    Execute command method created. FeatureExtraction method completed.

--- AlizePHP.php
+++ AlizePHP.php
@@ -5,21 +5,56 @@ namespace alizephp;
 class AlizePHP {
 	
 	private $speaker;
-	private $conf require 'config/alizephp_conf.php';
+	private $conf;
 	
 	private function getConfig() {
-		//$this->conf = require 'config/alizephp.php';
+		$this->conf = require 'config/alizephp_conf.php';
+	}
+	
+	private function executeCommand($comm) {
+		$descriptorspec = array(
+				0 => array("pipe", "r"),  // stdin
+				1 => array("pipe", "w"),  // stdout
+				2 => array("pipe", "w"),  // stderr
+		);
+		
+		$process = proc_open($comm, $descriptorspec, $pipes);
+		
+		$outvalues = array();
+		
+		// $outvalues[1] is stdout
+		$outvalues[1] = stream_get_contents($pipes[1]);
+		fclose($pipes[1]);
+		
+		// $outvalues[2] is stderr
+		$outvalues[2] = stream_get_contents($pipes[2]);
+		fclose($pipes[2]);
+		
+		// $outvalues[0] is return value
+		$outvalues[0] = proc_close($process);
+		
+		return $outvalues;
+		
+	}
+	
+	private function getBinPath() {
+		return $this->conf['base_bin_dir'];
+	}
+	
+	private function getBaseDataDir() {
+		return $this->conf['base_data_dir']
 	}
 	
 	public function getAudioFilePath() {
-		return $this->conf['base_dir'] . DIRECTORY_SEPARATOR . $this->conf['audio_dir'] . DIRECTORY_SEPARATOR . $this->speaker . ".pcm";
+		return $this->getBaseDataDir() . DIRECTORY_SEPARATOR . $this->conf['audio_dir'] . DIRECTORY_SEPARATOR . $this->speaker . ".pcm";
 	}
 	
 	public function getFeauresFilePath() {
-		return $this->conf['base_dir'] . DIRECTORY_SEPARATOR . $this->conf['features_dir'] . DIRECTORY_SEPARATOR . $this->speaker . ".prm";
+		return $this->getBaseDataDir() . DIRECTORY_SEPARATOR . $this->conf['features_dir'] . DIRECTORY_SEPARATOR . $this->speaker . ".prm";
 	}
 	
 	function __construct($speaker, $audio_file_path) {
+		$this->getConfig();
 		$this->speaker = $speaker;
 		if (!$speaker) Throw new AlizePHPException("Speaker must be a nonempty value.");
 		file_put_contents("data/pcm/".$this->speaker.".pcm", file_get_contents($audio_file_path));
@@ -27,9 +62,10 @@ class AlizePHP {
 	
 	function extractFeatures ($param_string = NULL) {
 		if ($param_string == NULL) $param_string = "-m -k 0.97 -p19 -n 24 -r 22 -e -D -A -F PCM16";
-		$command = "sfbcep " . $param_string . " ".$this->getAudioFilePath()." ".$this->getFeauresFilePath();
-		exec($command);
-		return fopen($this->getFeauresFilePath("r"));
+		$command = $this->getBinPath() . "sfbcep " . $param_string . " ".$this->getAudioFilePath()." ".$this->getFeauresFilePath();
+		$outvalues = $this->executeCommand($command);
+		if ($outvalues[0] != 0) throw new AlizePHPException($outvalues[2]);
+		return true;
 	}
 	
 }
--- config/alizephp_conf.php
+++ config/alizephp_conf.php
@@ -1,6 +1,7 @@
 <?php
 	return array (
-		'base_dir' => 'data',
+		'base_bin_dir' => DIRECTORY_SEPARATOR . 'usr'. DIRECTORY_SEPARATOR . "local" . DIRECTORY_SEPARATOR . "bin" . DIRECTORY_SEPARATOR,
+		'base_data_dir' => 'data',
 		'audio_dir' => 'pcm',
 		'features_dir' => 'prm'
 	);
