f450d0eccdfbad8a604b333a559a310700c39cb1
Héctor Pablos <hctpbl@gmail.com>
2014-08-23 02:03:50 +0200

    Added documentation to unit tests

--- tests/AlizePHPTest.php
+++ tests/AlizePHPTest.php
@@ -3,6 +3,12 @@ require __DIR__.'/../src/AlizePHP.php';
 
 use alizephp\AlizePHP;
 
+/**
+ * This class preforms the necessary unit tests in the AlizePHP class.s
+ * 
+ * @author Héctor Pablos
+ *
+ */
 class AlizePHPTest extends PHPUnit_Framework_TestCase {
 	
 	protected $nameOne = "usuarioUno";
@@ -12,6 +18,7 @@ class AlizePHPTest extends PHPUnit_Framework_TestCase {
 	protected $filePathTwo = 'tests/xaag.pcm';
 	
 	/**
+	 * Tries to create a user with an empty username
 	 * @expectedException \alizephp\AlizePHPException
 	 * @expectedExceptionMessage Speaker must be a nonempty value.
 	 */
@@ -20,6 +27,8 @@ class AlizePHPTest extends PHPUnit_Framework_TestCase {
 	}
 	
 	/**
+	 * Tries to create a username with a string containing only blank
+	 * characters
 	 * @expectedException \alizephp\AlizePHPException
 	 * @expectedExceptionMessage Speaker must be a nonempty value.
 	 */
@@ -28,6 +37,7 @@ class AlizePHPTest extends PHPUnit_Framework_TestCase {
 	}
 	
 	/**
+	 * Tries to create a user with an empty filename
 	 * @expectedException \alizephp\AlizePHPException
 	 * @expectedExceptionMessage Original audio file missing or unreadable.
 	 */
@@ -36,6 +46,8 @@ class AlizePHPTest extends PHPUnit_Framework_TestCase {
 	}
 	
 	/**
+	 * Tries to create a user with passing the name of a file that doesn't
+	 * exists
 	 * @expectedException \alizephp\AlizePHPException
 	 * @expectedExceptionMessage Original audio file missing or unreadable.
 	 */
@@ -44,6 +56,7 @@ class AlizePHPTest extends PHPUnit_Framework_TestCase {
 	}
 	
 	/**
+	 * Tries to create a user with the name of a file not readable by the process
 	 * @expectedException \alizephp\AlizePHPException
 	 * @expectedExceptionMessage Original audio file missing or unreadable.
 	 */
@@ -51,6 +64,10 @@ class AlizePHPTest extends PHPUnit_Framework_TestCase {
 		$emptyUsernameUsr = new AlizePHP($this->nameOne, "noPermissions.pcm");
 	}
 	
+	/**
+	 * Creates an AlizePHP user with correct values
+	 * @return \alizephp\AlizePHP
+	 */
 	public function testCreateUserSuccessful() {
 		$alizeuser = new AlizePHP($this->nameOne, $this->filePathOne);
 		$this->assertEquals($alizeuser->getSpeaker(), $this->nameOne);
@@ -60,7 +77,9 @@ class AlizePHPTest extends PHPUnit_Framework_TestCase {
 	}
 
 	/**
+	 * Tests the success of the extraction of features
 	 * @depends testCreateUserSuccessful
+	 * @return \alizephp\AlizePHP
 	 */
 	public function testExtractFeatures(AlizePHP $alizeuser) {
 		$alizeuser->extractFeatures();
@@ -70,7 +89,9 @@ class AlizePHPTest extends PHPUnit_Framework_TestCase {
 	}
 	
 	/**
+	 * Test the success of the normalisation of energy method
 	 * @depends testExtractFeatures
+	 * @return \alizephp\AlizePHP
 	 */
 	public function testNormaliseEnergy(AlizePHP $alizeuser) {
 		$alizeuser->normaliseEnergy();
@@ -80,6 +101,8 @@ class AlizePHPTest extends PHPUnit_Framework_TestCase {
 	}
 	
 	/**
+	 * Tries to normalise energy without having a features file for the
+	 * user
 	 * @expectedException \alizephp\AlizePHPException
 	 */
 	public function testFailNormaliseEnergy() {
@@ -88,7 +111,9 @@ class AlizePHPTest extends PHPUnit_Framework_TestCase {
 	}
 	
 	/**
+	 * Tests the success of the detection of energy method
 	 * @depends testNormaliseEnergy
+	 * @return \alizephp\AlizePHP
 	 */
 	public function testDetectEnergy(AlizePHP $alizeuser) {
 		$alizeuser->detectEnergy();
@@ -98,6 +123,8 @@ class AlizePHPTest extends PHPUnit_Framework_TestCase {
 	}
 	
 	/**
+	 * Tries to detect energy without having a normalised energy file for the
+	 * user
 	 * @expectedException \alizephp\AlizePHPException
 	 */
 	public function testFailDetectEnergy() {
@@ -106,7 +133,9 @@ class AlizePHPTest extends PHPUnit_Framework_TestCase {
 	}
 	
 	/**
+	 * Tests the normalisation of features method
 	 * @depends testDetectEnergy
+	 * @return \alizephp\AlizePHP
 	 */
 	public function testNormaliseFeatures(AlizePHP $alizeuser) {
 		$alizeuser->normaliseFeatures();
@@ -116,6 +145,8 @@ class AlizePHPTest extends PHPUnit_Framework_TestCase {
 	}
 	
 	/**
+	 * Tries to normalise features without a label file for the
+	 * user
 	 * @expectedException \alizephp\AlizePHPException
 	 */
 	public function testFailNormaliseFeatures() {
@@ -125,6 +156,7 @@ class AlizePHPTest extends PHPUnit_Framework_TestCase {
 
 
 	/**
+	 * Tests the success of the train target method
 	 * @depends testNormaliseFeatures
 	 */
 	public function testTrainTarget(AlizePHP $alizeuser) {
@@ -136,6 +168,8 @@ class AlizePHPTest extends PHPUnit_Framework_TestCase {
 	}
 	
 	/**
+	 * Tries to generate a model without having a normalised features
+	 * file for the user
 	 * @expectedException \alizephp\AlizePHPException
 	 */
 	public function testFailTrainTarget() {
@@ -144,6 +178,7 @@ class AlizePHPTest extends PHPUnit_Framework_TestCase {
 	}
 	
 	/**
+	 * Test the compute test method
 	 * @depends testTrainTarget
 	 */
 	public function testComputeTest(AlizePHP $alizeuser) {
@@ -162,9 +197,30 @@ class AlizePHPTest extends PHPUnit_Framework_TestCase {
 		
 		return $alizeuser;
 	}
-
+	
+	/**
+	 * Tries to use the computeTest method to test a user against
+	 * another, with the former not having a normalised features file
+	 * generated
+	 * @expectedException \alizephp\AlizePHPException
+	 */
+	public function testFailComputeTestNoFeatures() {
+		$otheruser = new AlizePHP($this->nameTwo, $this->filePathTwo);
+		$otheruser->trainTarget($this->nameOne);
+	}
+	
+	/**
+	 * Tries to use the computeTest method to test a user against
+	 * another, with the latter not having a model generated
+	 * @depends testComputeTest
+	 * @expectedException \alizephp\AlizePHPException
+	 */
+	public function testFailComputeTestNoModel(AlizePHP $alizeuser) {
+		$alizeuser->computeTest($this->nameTwo);
+	}
 
 	/**
+	 * Tests the removal of a user from the system
 	 * @depends testComputeTest
 	 */
 	public function testCleanUserFiles(AlizePHP $alizeuser) {
